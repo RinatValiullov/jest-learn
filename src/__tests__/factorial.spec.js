e609b128d740c83e3f020197fc1b5997047c2522
Rinat Valiullov <4rontender@gmail.com>
2021-11-10 15:02:14 +0500

    update dependencies, add "test:watch" and "test:coverage" npm scripts

--- src/__tests__/factorial.spec.js
+++ src/__tests__/factorial.spec.js
@@ -1,4 +1,4 @@
-import { factorial } from '../factorial';
+import { factorial } from "../factorial";
 
 describe("factorial()", () => {
   it("should calc 1! as 1", () => {
@@ -7,5 +7,9 @@ describe("factorial()", () => {
 
   it("should calc 3! as 6", () => {
     expect(factorial(3)).toBe(6);
+  });
+
+  it("should calc 5! as 120", () => {
+    expect(factorial(5)).toBe(120);
   });
 });
