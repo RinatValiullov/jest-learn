7626bc3560b0f896f06eb215125d53c263e3e190
Rinat Valiullov <4rontender@gmail.com>
2022-01-10 00:09:56 +0500

    add mock functions to test events in "Button" and "Input" components

--- src/__tests__/button.spec.js
+++ src/__tests__/button.spec.js
@@ -1,2 +1,13 @@
 import React from "react";
 import Button from "../Button/Button";
+import { shallow } from "enzyme";
+
+describe("Button component", () => {
+  it("should call onClick method", () => {
+    const mockCallBack = jest.fn();
+    const component = shallow(<Button onClick={mockCallBack} />);
+    expect(mockCallBack.mock.calls.length).toEqual(0);
+    component.find(".btn").simulate("click");
+    expect(mockCallBack.mock.calls.length).toEqual(1);
+  });
+});
