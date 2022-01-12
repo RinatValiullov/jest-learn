f8b5a76c899409ba8ee31aebae8aa2a7611aa4ec
Rinat Valiullov <4rontender@gmail.com>
2022-01-12 22:41:21 +0500

    update "Posts" component and tests, mockinf fetch function

--- src/__tests__/posts.spec.js
+++ src/__tests__/posts.spec.js
@@ -1,6 +1,19 @@
 import React from "react";
 import Posts from "../Posts/Posts";
 
+import {
+  NEWS,
+  BASE_PATH,
+  SEARCH_PATH,
+  SEARCH_PARAM,
+  PAGE_HITS,
+  PAGE_PARAM,
+} from "../Posts/constants";
+
+const mockJsonPromise = Promise.resolve({ hits: NEWS, page: 1, nbPages: 10 });
+const mockFetchPromise = Promise.resolve({ json: () => mockJsonPromise });
+global.fetch = jest.fn().mockImplementation(() => mockFetchPromise);
+
 const setupComponent = () => shallow(<Posts />);
 
 describe("Posts component", () => {
@@ -13,8 +26,57 @@ describe("Posts component", () => {
     instance = component.instance();
   });
 
-  it("should render Post component", () => {
+  it("should render Posts component", () => {
     expect(component).toMatchSnapshot();
+  });
+
+  it("should call fetch in componentDidMount", () => {
+    expect(fetch).toHaveBeenCalledWith(
+      `${BASE_PATH}${SEARCH_PATH}?${SEARCH_PARAM}${""}&${PAGE_HITS}${20}&${PAGE_PARAM}${0}`
+    );
+  });
+
+  describe("updatePageMethod", () => {
+    it("should update state 'page' value", () => {
+      instance.updatePage(DEFAULT_PAGE);
+      expect(component.state().page).toBe(DEFAULT_PAGE);
+    });
+
+    it("should call fetch with given argument", () => {
+      instance.updatePage(DEFAULT_PAGE);
+      expect(global.fetch).toHaveBeenCalledWith(
+        `${BASE_PATH}${SEARCH_PATH}?${SEARCH_PARAM}${""}&${PAGE_HITS}${20}&${PAGE_PARAM}${DEFAULT_PAGE}`
+      );
+    });
+  });
+
+  describe("handlePageChange method", () => {
+    it("should call 'updatePage' with given argument", () => {
+      instance.updatePage = jest.fn();
+      instance.setState({ page: DEFAULT_PAGE });
+      instance.handlePageChange({
+        target: { getAttribute: jest.fn().mockReturnValue("1") },
+      });
+      expect(instance.updatePage).toHaveBeenCalledWith(1);
+    });
+
+    it("should call 'updatePage' with increased value", () => {
+      instance.updatePage = jest.fn();
+      instance.setState({ page: DEFAULT_PAGE });
+      instance.handlePageChange({
+        target: { getAttribute: jest.fn().mockReturnValue("prev") },
+      });
+      expect(instance.updatePage).toHaveBeenCalledWith(DEFAULT_PAGE - 1);
+    });
+
+    it("should call 'updatePage' with decreased value", () => {
+      instance.updatePage = jest.fn();
+      instance.setState({ page: DEFAULT_PAGE });
+      instance.handlePageChange({
+        target: { getAttribute: jest.fn().mockReturnValue("next") },
+      });
+      expect(instance.updatePage).toHaveBeenCalledWith(DEFAULT_PAGE + 1);
+    });
   });
 
   describe("Posts handlers", () => {
